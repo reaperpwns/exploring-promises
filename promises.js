f53560f22318ba719254545d7f340062d43b4413
Tony Vongvone <tvongvone@gmail.com>
2022-10-19 15:35:35 -0700

    phase 2 complete

--- promises.js
+++ promises.js
@@ -2,14 +2,27 @@
 /* -------------------------- exploring async -------------------------- */
 
 // Your code here
+function num1() {
+    return 1;
+}
+async function num2() {
+    return 2;
+}
 
+console.log('num1', num1());
+console.log('num2', num2().then(result => console.log(result)));
 
 
 /* ============================== Phase 2 ============================== */
 /* -------------------------- exploring await -------------------------- */
 
 // Your code here
+async function wait() {
+    const value = await num2();
+    console.log('waiting', value)
+}
 
+wait();
 
 
 /* ============================== Phase 3 ============================== */
